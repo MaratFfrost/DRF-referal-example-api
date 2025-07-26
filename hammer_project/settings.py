5be916f534430a4a06653b8cb3ecde6f2e6bcf67
Marat Frost <morozovmarat4@gmail.com>
2025-07-26 04:42:03 +0300

    Update settings.py

--- hammer_project/settings.py
+++ hammer_project/settings.py
@@ -31,15 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-]
+CSRF_TRUSTED_ORIGINS = []
 
-CORS_ALLOWED_ORIGINS = [
-  "http://localhost:5500",
-  "http://127.0.0.1:5500",
-]
+CORS_ALLOWED_ORIGINS = []
 CORS_ALLOW_CREDENTIALS = True
 
 
