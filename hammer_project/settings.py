98ee63ecedb79b6cccf8af358c7bb95757c379a1
Marat Frost <morozovmarat4@gmail.com>
2025-07-26 03:41:04 +0300

    Update settings.py

--- hammer_project/settings.py
+++ hammer_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["drf-referal-example-api.onrender.com"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5500",
